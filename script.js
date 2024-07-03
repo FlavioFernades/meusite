ad2799cb5490dc8cd1925a4d1aa9e61639c16ddf
FlavioFernades <flavioifrn@gmail.com>
2024-07-03 20:44:13 -0300

    script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const h2Elements = document.querySelectorAll('h2');
   let avatarToggle = false;
 
-  // Toggle avatar image on click
+  
   avatar.addEventListener('click', function() {
     avatar.src = avatarToggle ? 'avatar.jpg' : 'avatar2.jpg'; // Change 'avatar2.jpg' to the path of the second image
     avatarToggle = !avatarToggle;
   });
-
-  // Toggle visibility of section content on click/double click
   h2Elements.forEach(function(h2) {
     h2.addEventListener('click', function() {
       const content = h2.nextElementSibling;
@@ -19,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
         content.style.display = 'none';
       }
     });
-
     h2.addEventListener('dblclick', function() {
       const content = h2.nextElementSibling;
       content.style.display = 'block';
