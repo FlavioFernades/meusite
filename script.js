f0d093b6774218f33c8fd3ee48e75556ecd8f9d4
FlavioFernades <flavioifrn@gmail.com>
2024-07-03 20:21:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const h2Elements = document.querySelectorAll('h2');
   let avatarToggle = false;
 
+  // Toggle avatar image on click
   avatar.addEventListener('click', function() {
-    avatar.src = avatarToggle ? 'avatar.jpg' : 'avatar2.jpg'; 
+    avatar.src = avatarToggle ? 'avatar.jpg' : 'avatar2.jpg'; // Change 'avatar2.jpg' to the path of the second image
     avatarToggle = !avatarToggle;
   });
 
+  // Toggle visibility of section content on click/double click
   h2Elements.forEach(function(h2) {
     h2.addEventListener('click', function() {
       const content = h2.nextElementSibling;
-      content.style.display = 'none';
+      if (content.style.display === 'none') {
+        content.style.display = 'block';
+      } else {
+        content.style.display = 'none';
+      }
     });
 
     h2.addEventListener('dblclick', function() {
